0d1db33412d8cf6218f0141debefd8d5a76fa3f5
Chelen <pierrick.brunet@telecom-bretagne.eu>
2012-06-14 12:15:20 +0200

    Undo/redo enable for pdf action

--- Scribus/scribus/pageitem.h
+++ Scribus/scribus/pageitem.h
@@ -1180,6 +1180,7 @@ public:
 	
 	bool isAnnotation() const { return m_isAnnotation; }
 	void setIsAnnotation(bool);
+	void setIsBookMark(bool);
 	void setAnnotation(const Annotation& ad);
 	Annotation& annotation() { return m_annotation; }
 	const Annotation& annotation() const { return m_annotation; }
