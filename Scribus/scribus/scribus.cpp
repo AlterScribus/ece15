d9fef68f16527a9aaafc2fe952ee23947b0901fc
cezaryece <czarek@oferuje.pl>
2012-07-30 13:21:33 +0200

    redone

--- Scribus/scribus/scribus.cpp
+++ Scribus/scribus/scribus.cpp
@@ -1394,8 +1394,6 @@ void ScribusMainWindow::keyPressEvent(QKeyEvent *k)
 				case modeCopyProperties:
 				case modeEditGradientVectors:
 				case modeEditMeshGradient:
-				case modeLinkFrames:
-				case modeUnlinkFrames:
 				case modeRotation:
 					view->Deselect(false);
 				case modeLinkFrames:
