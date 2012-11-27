4944df558fd8c195a6ea111133e56a88dc8d3056
cezaryece <czarek@oferuje.pl>
2012-11-27 15:39:12 +0100

    Delete selected text in bullet string LineEdit on inserting special chars from chars palette.

--- Scribus/scribus/ui/propertywidget_pareffect.cpp
+++ Scribus/scribus/ui/propertywidget_pareffect.cpp
@@ -674,5 +674,7 @@ void PropertyWidget_ParEffect::on_bulletCharTableButton__toggled(bool checked)
 }
 void PropertyWidget_ParEffect::insertSpecialChars(const QString &chars)
 {
+	if (bulletStrEdit_->lineEdit()->hasSelectedText())
+		bulletStrEdit_->lineEdit()->del();
 	bulletStrEdit_->lineEdit()->insert(chars);
 }
--- Scribus/scribus/ui/smpstylewidget.cpp
+++ Scribus/scribus/ui/smpstylewidget.cpp
@@ -1074,6 +1074,8 @@ void SMPStyleWidget::slotBullets(bool isOn)
 
 void SMPStyleWidget::insertSpecialChars(const QString &chars)
 {
+	if (bulletStrEdit_->lineEdit()->hasSelectedText())
+		bulletStrEdit_->lineEdit()->del();
 	bulletStrEdit_->lineEdit()->insert(chars);
 }
 
